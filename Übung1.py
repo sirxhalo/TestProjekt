14617988028e3f89be488886d804d5a84b9317ca
tim <127527320+sirxhalo@users.noreply.github.com>
2024-06-01 21:32:47 +0200

    Update Übung1.py

--- Übung1.py
+++ Übung1.py
@@ -1,10 +1,10 @@
-stundenlohn = input("Bitte gebe deinen Stundenlohn ein: ")
-tag = 8 * int(stundenlohn)
-monat = tag * 20
-year = monat * 12
-print("Dein Stundenlohn beträgt " + str(stundenlohn) + "€")
-print("Du verdienst " + str(tag) + "€ pro Tag")
-print("Du verdienst " + str(monat) + "€ pro Monat")
+hourly_wage = input("Bitte gebe deinen Stundenlohn ein: ")
+day = 8 * int(hourly_wage)
+month = day * 20
+year = month * 12
+print("Dein Stundenlohn beträgt " + str(hourly_wage) + "€")
+print("Du verdienst " + str(day) + "€ pro Tag")
+print("Du verdienst " + str(month) + "€ pro Monat")
 print("Du hast ein Jahresgehalt von " + str(year) + "€")
 print("")
 input("Bitte Beliebige Taste drücken...")
